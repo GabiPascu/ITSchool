ad69afa86c75f90085d3356cad14ce50df2edde6
GabiPascu <pop_gabi00@yahoo.com>
2023-09-06 14:06:50 +0300

    new branch created

---
+++ src/aSuitableName.java
@@ -0,0 +1,6 @@
+public class aSuitableName {
+        public static void main(String[] args) {
+            System.out.println("I have created a new branch on which we will be working on");
+        }
+
+}
