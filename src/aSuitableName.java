dd959abe0ed6e694582ace614d6b2803566a747a
GabiPascu <pop_gabi00@yahoo.com>
2023-09-08 19:48:14 +0300

    Created session2 package

--- src/aSuitableName.java
+++
@@ -1,6 +0,0 @@
-public class aSuitableName {
-        public static void main(String[] args) {
-            System.out.println("I have created a new branch on which we will be working on");
-        }
-
-}
---
+++ src/session2.java
@@ -0,0 +1,19 @@
+public class session2 {
+    public static class Test {
+    }
+
+    public static class Main {
+        public static void main(String[] args) {
+            System.out.println("Hello world!");
+            System.out.println("Another hello");
+            System.out.println("I have created a new branch on which we will be working on");
+        }
+    }
+
+    public static class aSuitableName {
+            public static void main(String[] args) {
+                System.out.println("I have created a new branch on which we will be working on");
+            }
+
+    }
+}
