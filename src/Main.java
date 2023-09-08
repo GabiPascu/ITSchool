1855669cf204c34b3ae1451e05b832285d222c49
GabiPascu <144155356+GabiPascu@users.noreply.github.com>
2023-09-08 18:37:16 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Another hello");
+        System.out.println("Another another hello");
         System.out.println("I have created a new branch on which we will be working on");
     }
 }
