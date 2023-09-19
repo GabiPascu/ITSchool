2b079032ce839816e195e0b7d9b337cddc72fc5d
GabiPascu <pop_gabi00@yahoo.com>
2023-09-19 18:29:56 +0300

    Session5 challenges

--- src/session5/challenges/Challenge6.java
+++ src/session5/challenges/Challenge6.java
@@ -1,28 +1,31 @@
 package session5.challenges;
 
 import java.util.Scanner;
-//WIP
+
+
 //Write a program that asks the user to enter numbers. The program should continue prompting the user until they enter -1. After they enter -1, print the sum of all numbers entered (excluding -1).
 public class Challenge6 {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Please enter a number: ");
-        int number = scanner.nextInt();
-        positiveNumber(number);
-        while (number > -1) {
-            System.out.println("Please enter another number: ");
-        }
-    }
-
-    public static void positiveNumber(int number) {
         int sum = 0;
-if (number == -1) {
-    System.out.println(sum += number);
-}
 
+        System.out.println("Enter numbers (enter -1 to calculate the sum):");
+
+        int number;
+        while (true) {
+            System.out.print("Enter a number: ");
+            number = scanner.nextInt();
+
+            if (number == -1) {
+                break; // Exit the loop when -1 is entered
+            }
+
+            sum += number; // Add the entered number to the sum
+        }
+
+        System.out.println("Sum of the entered numbers (excluding -1): " + sum);
+        scanner.close();
     }
 }
 
---
+++ src/session5/challenges/Challenge7.java
@@ -0,0 +1,11 @@
+package session5.challenges;
+
+//Create a program where the user is prompted to guess a predefined number.
+// After each guess, the program should inform the user if the guess is too high or too low.
+// The program should keep prompting the user until they guess correctly. Using a do-while loop, ensure the user is always asked at least once.
+public class Challenge7 {
+    public static void main(String[] args) {
+
+
+    }
+}
