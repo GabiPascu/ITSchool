037f1b5ce39ef1b8ff550bc1bfb7d7e4c320044e
GabiPascu <pop_gabi00@yahoo.com>
2023-10-31 15:49:21 +0200

    session17 challenges

---
+++ src/session17_streams/challenges/challenge12/Employee.java
@@ -0,0 +1,38 @@
+package session17_streams.challenges.challenge12;
+
+public class Employee {
+
+    private String name;
+    private String department;
+    private int salary;
+
+    public Employee(String name, String department, int salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
---
+++ src/session17_streams/challenges/challenge12/RichPeopleFinder.java
@@ -0,0 +1,8 @@
+package session17_streams.challenges.challenge12;
+
+public class RichPeopleFinder {
+
+    public static void main(String[] args) {
+
+    }
+}
